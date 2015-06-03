32aefe938743c7a79a62e8bff5bef9847e1bd904
carlos <kyncarlopez@gmail.com>
2015-06-03 09:18:35 +0200

    Cambios en tutorial django

--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -1,5 +1,5 @@
 # Create your views here.
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response,get_object_or_404
 from django.template import Context, loader
 from django.http import HttpResponse
 from polls.models import Poll
@@ -10,11 +10,8 @@ def index(request):
     return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
 
 def detail(request, poll_id):
-    try:
-        p = Poll.objects.get(pk=poll_id)
-    except Poll.DoesNotExist:
-        raise Http404
-    return render_to_response('polls/detail.html', {'poll' : p})
+    p = get_object_or_404(Poll, pk=poll_id)
+    return render_to_response('polls/detail.html',{'poll':p})
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
