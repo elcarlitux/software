af21a1545d376366527464fa8a69ed550a18140d
carlos <kyncarlopez@gmail.com>
2015-06-09 17:55:04 +0200

    Mysite with bookhandler

--- django/mysite2/mysite2/urls.py
+++ django/mysite2/mysite2/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
         url(r'^polls/', include('polls.urls', namespace="polls")),
+        url(r'^bookhandler/', include('bookhandler.urls', namespace="bookhandler")),
         url(r'^admin/', include(admin.site.urls)),
         ]
